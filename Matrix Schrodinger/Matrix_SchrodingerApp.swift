e500c94d793fb1a589d08e524a30fc1b9a9f1626
Dbitler <dbitler@hawk.iit.edu>
2023-09-22 16:11:03 -0500

    Initial Commit

---
+++ Matrix Schrodinger/Matrix_SchrodingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_SchrodingerApp.swift
+//  Matrix Schrodinger
+//
+//  Created by IIT PHYS 440 on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_SchrodingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Matrix SchrodingerUITests/Matrix_SchrodingerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Matrix_SchrodingerUITestsLaunchTests.swift
+//  Matrix SchrodingerUITests
+//
+//  Created by IIT PHYS 440 on 9/22/23.
+//
+
+import XCTest
+
+final class Matrix_SchrodingerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
