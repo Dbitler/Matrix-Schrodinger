3358cc01cb2e5d3ff5f7fc9dace0d5487dc234d4
Dbitler <dbitler@hawk.iit.edu>
2023-09-22 17:18:23 -0500

    test

--- Matrix Schrodinger/ContentView.swift
+++ Matrix Schrodinger/ContentView.swift
@@ -3,8 +3,10 @@
 //  Matrix Schrodinger
 //
 //  Created by IIT PHYS 440 on 9/22/23.
-//
-
+/* Write a program that calculates the solution to the 1D Shrodinger Equation using the matrix form of the equation. Expand in terms of the known 1D particle in a box solutions to solve the matrix equation. The program should be able to load in an arbitrary 1D potential to be solved. Use the same example potentials from Problem 4.
+ 
+ 1d particle in a box = infinite square well, but have to expand to other potentials. Infinite square well has a potential energy of 0.
+ */
 import SwiftUI
 
 struct ContentView: View {
